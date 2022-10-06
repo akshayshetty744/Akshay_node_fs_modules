6a4332c4eaa8b806ec30913049cf741377a591ce
akshayshetty744 <shettyakshay744@gmail.com>
2022-10-06 15:50:19 +0530

    removed Promise

--- Problem_0.2.js
+++ Problem_0.2.js
@@ -1,19 +1,31 @@
 const fs = require('fs')
-function dataDeleter() {
-    return Promise.all(
-      ['./a.json', './b.json', './c.json'].map(
-        file =>
-          new Promise((res, rej) => {
-            try {
-              fs.unlink(`./${file}`, err => {
-                if (err) throw err;
-                console.log(`${file}.json was deleted`);
-              });
-            }
-            catch (err) {
-                console.log(err)
-            }
-          })//promiss
-    ))//map
-  };
-  dataDeleter();
+// function dataDeleter() {
+//     return Promise.all(
+//       ['./a.json', './b.json', './c.json'].map(
+//         file =>
+//           new Promise((res, rej) => {
+//             try {
+//               fs.unlink(`./${file}`, err => {
+//                 if (err) throw err;
+//                 console.log(`${file}.json was deleted`);
+//               });
+//             }
+//             catch (err) {
+//                 console.log(err)
+//             }
+//           })//promiss
+//     ))//map
+//   };
+//   dataDeleter();
+
+function f1(){
+   var data =  ['./a.json', './b.json'];
+    data.map((e)=>{
+        fs.unlink(`./${e}`, err => {
+            if (err) throw err;
+            console.log(`${data}.json was deleted`);
+          });
+    }
+    )
+}
+f1();
